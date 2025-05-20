4e19a44a0f19bb6602d4764fa76865c1a09e112e
Erkhembayar <erhenn090@gmail.com>
2025-05-20 10:24:31 +0800

    reset

--- assignment_2/frontend/pages/components/api.js
+++ assignment_2/frontend/pages/components/api.js
@@ -64,9 +64,3 @@ export async function getCartCount() {
   // console.log("Cart count: 0");
   return 0;
 }
-
-export async function fetchProductsDetails() {
-  const response = await fetch("./productDetails.json");
-  if (!response.ok) throw new Error("Failed to fetch all products from URL");
-  return await response.json();
-}
