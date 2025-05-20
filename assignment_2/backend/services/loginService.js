ed451ec1c0d09adf3fa971d0a6033be18d7df9d4
Zadyy <nyambayar2014@gmail.com>
2025-05-21 02:18:18 +0800

    testing new base root on vm

---
+++ assignment_2/backend/services/loginService.js
@@ -0,0 +1,18 @@
+import sql from "../database/database";
+import crypto from "crypto";
+
+function sha256(value) {
+    return crypto.createHash("sha256").update(value).digest("hex");
+}
+
+export async function checkUsername(username) {
+    const hash = sha256(username);
+    const result = await sql`select checkUserName(${hash})`;
+    return result.length > 0 ? true : false;
+}
+
+export async function checkPassword(password) {
+    const hash = sha256(password);
+    const result = await sql`select checkUserPassword(${hash})`;
+    return result.length > 0 ? true : false;
+}
